60eb0cf2ff2b2656cc41ea26bf27ff8186214165
Sreeni <kattubadi426@gmail.com>
2024-04-28 11:30:56 +0530

    Added validation libraries

---
+++ main2.py
@@ -0,0 +1,13 @@
+import os
+
+
+batch_date = os.getenv('BATCH_DATE')
+table = os.getenv('TABLE')
+
+# Print batch_date and table
+print("Batch Date:", batch_date)
+print("Table Name:", table)
+
+
+
+
